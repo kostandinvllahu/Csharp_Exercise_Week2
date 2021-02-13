19fbf8dd15a9b1373a04498aebb37ce7ceabdfa9
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-13 14:00:58 +0100

    Stack Exercise Finished

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,92 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StackImplementation
 {
-    internal class Stack
-    {
-        static readonly int MAX = 1000;
-        int top;
-        int[] stack = new int[MAX];
+    
 
-        bool IsEmpty()
-        {
-            return (top < 0);
-        }
-        public Stack()
-        {
-            top = -1;
-        }
-        internal bool Push(int data)
-        {
-            if (top >= MAX)
-            {
-                Console.WriteLine("Stack Overflow");
-                return false;
-            }
-            else
-            {
-                stack[++top] = data;
-                return true;
-            }
-        }
-
-        internal int Pop()
-        {
-            if (top < 0)
-            {
-                Console.WriteLine("Stack Underflow");
-                return 0;
-            }
-            else
-            {
-                int value = stack[top--];
-                return value;
-            }
-        }
-
-        internal void Peek()
-        {
-            if (top < 0)
-            {
-                Console.WriteLine("Stack Underflow");
-                return;
-            }
-            else
-                Console.WriteLine("The topmost element of Stack is : {0}", stack[top]);
-        }
-
-        internal void PrintStack()
-        {
-            if (top < 0)
-            {
-                Console.WriteLine("Stack Underflow");
-                return;
-            }
-            else
-            {
-                Console.WriteLine("Items in the Stack are :");
-                for (int i = top; i >= 0; i--)
-                {
-                    Console.WriteLine(stack[i]);
-                }
-            }
-        }
-    }
 
     class Program
     {
+
+    
+        
+        public static void Push()
+        {
+            Stack<int> stk = new Stack<int>();
+            stk.Push(10);
+            stk.Push(20);
+            stk.Push(30);
+            stk.Push(40);
+            Console.WriteLine(" Stack is pushed");
+           foreach (Object obj in stk)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+        
+        public static void Pop()
+        {
+            Stack<int> stk = new Stack<int>();
+            stk.Push(10);
+            stk.Push(20);
+            stk.Push(30);
+            stk.Push(40);
+            Console.WriteLine("The pop stack");
+            Console.WriteLine("POP STACK IS: " + stk.Pop());
+            foreach (Object obj in stk)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+
+        public static void Peep()
+        {
+            Stack<int> stk = new Stack<int>();
+            stk.Push(10);
+            stk.Push(20);
+            stk.Push(30);
+            stk.Push(40);
+            Console.WriteLine("The peep stack");
+            Console.WriteLine("PEEP STACK IS: " + stk.Peek());
+            foreach (Object obj in stk)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Stack myStack = new Stack();
-
-            myStack.Push(10);
-            myStack.Push(20);
-            myStack.Push(30);
-            myStack.Push(40);
-            myStack.PrintStack();
-            myStack.Peek();
-            Console.WriteLine("Item popped from Stack : {0}", myStack.Pop());
-            myStack.PrintStack();
+            Console.WriteLine("Stack Exercise");
+            Push();
+            Pop();
+            Peep();
         }
     }
 }
